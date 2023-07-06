3292a5e047f636e51d5b9f03541d3daf067e3a9f
Saied Sani <saied.sani.biderooni@gmail.com>
2023-07-06 22:51:38 +0200

    Component Rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Home from "./portfolioContainer/home/Home";
+import PortfolioContainer from "./portfolioContainer/PortfolioContainer";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <PortfolioContainer />
     </div>
   );
 }
