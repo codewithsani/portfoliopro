f1cddbe0037e1b1df6d58dd15736606a4d13673a
Saied Sani <saied.sani.biderooni@gmail.com>
2023-07-08 01:24:01 +0200

    Header Navigation and Click Functionality

--- client/src/portfolioContainer/home/Home.jsx
+++ client/src/portfolioContainer/home/Home.jsx
@@ -1,9 +1,11 @@
 import Profile from "./profile/Profile";
 import Footer from "./footer/Footer";
+import Header from "./header/Header";
 import "./home.css";
 export default function Home() {
   return (
     <div className="home-container">
+      <Header />
       <Profile />
       <Footer />
     </div>
